244c0ef1cee2aeec3e541a31289552472f3d7742
Alex S. Garzao <alexgarzao@gmail.com>
2016-06-07 11:25:48 -0300

    Refactor: A better error treatment in the main.

--- generator/main.go
+++ generator/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
 )
 
 func main() {
-	fmt.Printf("Starting ms-gen...\n")
+	log.Printf("Starting ms-gen...\n")
 
 	api := NewApi("../generator_tests/api_sample_1.yaml")
+	if err := api.LoadFromSwagger(); err != nil {
+		log.Fatalf("When loading swagger file %s: %s", api.Filename, err)
+	}
 
 	source := NewSource(api, "../service_templates/Makefile.tpl")
-	source.SaveToFile("./code/Makefile")
+	if err := source.SaveToFile("./code/Makefile"); err != nil {
+		log.Fatalf("Error when saving Makefile: %s", err)
+	}
 
 	source = NewSource(api, "../service_templates/SERVICE_NAME_common/definitions.go.tpl")
-	source.SaveToFile("./code/{{.ServiceName}}_common/definitions.go")
+	if err := source.SaveToFile("./code/{{.ServiceName}}_common/definitions.go"); err != nil {
+		log.Fatalf("Error when saving definitions.go: %s", err)
+	}
 
 	source = NewSource(api, "../service_templates/SERVICE_NAME_common/requests.go.tpl")
-	source.SaveToFile("./code/{{.ServiceName}}_common/requests.go")
+	if err := source.SaveToFile("./code/{{.ServiceName}}_common/requests.go"); err != nil {
+		log.Fatalf("Error when saving requests.go: %s", err)
+	}
 
-	fmt.Printf("Finishing ms-gen...\n")
+	log.Printf("Finishing ms-gen...\n")
 }
