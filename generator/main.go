25c28cdaac1983a09760fb4abb7922b30424ca1b
Alex S. Garzao <alexgarzao@gmail.com>
2016-06-08 14:42:43 -0300

    Refactor: Code moved from main to Source.Build method.

--- generator/main.go
+++ generator/main.go
@@ -27,50 +27,9 @@ func main() {
 		log.Fatalf("When loading swagger file %s: %s", api.Filename, err)
 	}
 
-	source := NewSource(api, "../service_templates/Makefile.tpl")
-	if err := source.SaveToFile("Makefile"); err != nil {
-		log.Fatalf("Error when saving Makefile: %s", err)
-	}
-
-	source = NewSource(api, "../service_templates/SERVICE_NAME_common/definitions.go.tpl")
-	if err := source.SaveToFile("{{.ServiceName}}_common/definitions.go"); err != nil {
-		log.Fatalf("Error when saving definitions.go: %s", err)
-	}
-
-	source = NewSource(api, "../service_templates/SERVICE_NAME_common/requests.go.tpl")
-	if err := source.SaveToFile("{{.ServiceName}}_common/requests.go"); err != nil {
-		log.Fatalf("Error when saving requests.go: %s", err)
-	}
-
-	source = NewSource(api, "../service_templates/SERVICE_NAME_server/db.go.tpl")
-	if err := source.SaveToFile("{{.ServiceName}}_server/db.go"); err != nil {
-		log.Fatalf("Error when saving db.go: %s", err)
-	}
-
-	source = NewSource(api, "../service_templates/SERVICE_NAME_server/db_models.go.tpl")
-	if err := source.SaveToFile("{{.ServiceName}}_server/db_models.go"); err != nil {
-		log.Fatalf("Error when saving db_models.go: %s", err)
-	}
-
-	source = NewSource(api, "../service_templates/SERVICE_NAME_server/main.go.tpl")
-	if err := source.SaveToFile("{{.ServiceName}}_server/main.go"); err != nil {
-		log.Fatalf("Error when saving main.go: %s", err)
-	}
-
-	source = NewSource(api, "../service_templates/SERVICE_NAME_server/service.go.tpl")
-	if err := source.SaveToFile("{{.ServiceName}}_server/service.go"); err != nil {
-		log.Fatalf("Error when saving service.go: %s", err)
-	}
-
-	source = NewSource(api, "../service_templates/SERVICE_NAME_server/get_method.go.tpl")
-	api.CurrentPath = api.Paths[0]
-	if err := source.SaveToFile("{{.ServiceName}}_server/" + api.CurrentPath.CodeFilename); err != nil {
-		log.Fatalf("Error when saving %s: %s", api.CurrentPath.CodeFilename, err)
-	}
-
-	source = NewSource(api, "../service_templates/SERVICE_NAME_server/SERVICE_NAME_config.yaml.example.tpl")
-	if err := source.SaveToFile("{{.ServiceName}}_server/{{.ServiceName}}_config.yaml.example"); err != nil {
-		log.Fatalf("Error when saving SERVICE_NAME_config.yaml.example: %s", err)
+	projectSource := NewSource(api)
+	if err := projectSource.Build(); err != nil {
+		log.Fatalf("When build source files: %s", err)
 	}
 
 	log.Printf("Finishing ms-gen...\n")
--- generator/source.go
+++ generator/source.go
@@ -10,25 +10,66 @@ import (
 )
 
 type Source struct {
-	api  *Api
-	tmpl *template.Template
+	api *Api
 }
 
-func NewSource(api *Api, templateFilename string) *Source {
+func NewSource(api *Api) *Source {
 	source := new(Source)
 
 	source.api = api
-	source.tmpl = template.Must(template.ParseGlob(templateFilename))
 
 	return source
 }
 
-func (s *Source) SaveToFile(templateFilename string) error {
+func (s *Source) Build() error {
+	if err := s.saveToFile("../service_templates/Makefile.tpl", "Makefile"); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving Makefile: %s", err))
+	}
+
+	if err := s.saveToFile("../service_templates/SERVICE_NAME_common/definitions.go.tpl", "{{.ServiceName}}_common/definitions.go"); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving definitions.go: %s", err))
+	}
+
+	if err := s.saveToFile("../service_templates/SERVICE_NAME_common/requests.go.tpl", "{{.ServiceName}}_common/requests.go"); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving requests.go: %s", err))
+	}
+
+	if err := s.saveToFile("../service_templates/SERVICE_NAME_server/db.go.tpl", "{{.ServiceName}}_server/db.go"); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving db.go: %s", err))
+	}
+
+	if err := s.saveToFile("../service_templates/SERVICE_NAME_server/db_models.go.tpl", "{{.ServiceName}}_server/db_models.go"); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving db_models.go: %s", err))
+	}
+
+	if err := s.saveToFile("../service_templates/SERVICE_NAME_server/main.go.tpl", "{{.ServiceName}}_server/main.go"); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving main.go: %s", err))
+	}
+
+	if err := s.saveToFile("../service_templates/SERVICE_NAME_server/service.go.tpl", "{{.ServiceName}}_server/service.go"); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving service.go: %s", err))
+	}
+
+	s.api.CurrentPath = s.api.Paths[0]
+	if err := s.saveToFile("../service_templates/SERVICE_NAME_server/get_method.go.tpl", "{{.ServiceName}}_server/"+s.api.CurrentPath.CodeFilename); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving %s: %s", s.api.CurrentPath.CodeFilename, err))
+	}
+
+	if err := s.saveToFile("../service_templates/SERVICE_NAME_server/SERVICE_NAME_config.yaml.example.tpl", "{{.ServiceName}}_server/{{.ServiceName}}_config.yaml.example"); err != nil {
+		return errors.New(fmt.Sprintf("Error when saving SERVICE_NAME_config.yaml.example: %s", err))
+	}
+
+	return nil
+}
+
+func (s *Source) saveToFile(templateFilename string, outputFilename string) error {
+
+	tmpl := template.Must(template.ParseGlob(templateFilename))
 
 	baseSourceDir := s.api.OutputDir + "/"
 
 	// Replace tokens in filename.
-	t := template.Must(template.New("template_filename").Parse(templateFilename))
+	t := template.Must(template.New("output_filename").Parse(outputFilename))
 
 	buffFilename := bytes.NewBufferString("")
 	t.Execute(buffFilename, s.api)
@@ -48,7 +89,7 @@ func (s *Source) SaveToFile(templateFilename string) error {
 
 	defer f.Close()
 
-	err = s.tmpl.Execute(f, s.api)
+	err = tmpl.Execute(f, s.api)
 	if err != nil {
 		return errors.New(fmt.Sprintf("template execution: %s", err))
 	}
