c643638e86b5244f30be68e8f58122e7c559b27f
Alex S. Garzao <alexgarzao@gmail.com>
2016-06-08 00:59:42 -0300

    Two news functions in util.
    1) Function to get the import base path (based on absolute path)
    2) Function to get the common import path (based on output dir)

--- generator/util.go
+++ generator/util.go
@@ -6,6 +6,7 @@ import (
 
 	"errors"
 	"fmt"
+	"strings"
 )
 
 func CreateBasePath(filename string) error {
@@ -16,4 +17,31 @@ func CreateBasePath(filename string) error {
 	}
 
 	return nil
+}
+
+func GetImportBasePath(absolutePath string) (string, error) {
+	n := strings.Index(absolutePath, "/github.com/")
+	if n == -1 {
+		return "", errors.New(fmt.Sprintf("Path github.com not found in %s", absolutePath))
+	}
+
+	return absolutePath[n+1:], nil
+}
+
+func GetCommonImportPath(outputDir string, serviceName string) (string, error) {
+	baseSourceDir := outputDir + "/"
+
+	absPath, err := filepath.Abs(baseSourceDir)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("When get abs(%s): %s", baseSourceDir, err))
+	}
+
+	importBasePath, err := GetImportBasePath(absPath)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("When getting import path from %s: %s", absPath, err))
+	}
+
+	commonImportPath := importBasePath + "/" + serviceName + "_common"
+
+	return commonImportPath, nil
 }
